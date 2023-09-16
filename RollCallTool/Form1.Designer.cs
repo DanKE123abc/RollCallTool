0d601b2874f5c72081fe71fda61b30134b7fdf8e
DanKe <1586017466@qq.com>
2023-09-16 22:08:37 +0800

    v1.0

---
+++ RollCallTool/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RollCallTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Microsoft YaHei UI", 135F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(-10, -6);
+            label1.Name = "label1";
+            label1.Size = new Size(1040, 385);
+            label1.TabIndex = 0;
+            label1.Text = "张三二一";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(-10, 382);
+            button1.Name = "button1";
+            button1.Size = new Size(1040, 173);
+            button1.TabIndex = 1;
+            button1.Text = "抽取一个";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1021, 548);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "一个很厉害但又没有那么厉害的点名器";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ RollCallTool/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DanKeJson;
+
+namespace RollCallTool
+{
+    public partial class Form1 : Form
+    {
+        string lastName = null;
+        string[] titles = { "一个很厉害又没有那么厉害的点名器", "世界第一的点名器", "二零二六届八班的dyc开发的点名器", "一个超简陋的点名器" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = RandomName();
+            while (name == lastName)
+            {
+                name = RandomName();
+            }
+            lastName = name;
+            label1.Text = name;
+        }
+
+        private string RandomName()
+        {
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "list.json");
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            List<JsonData> names = JSON.ToData(jsonContent).array;
+            Random random = new Random();
+            int index = random.Next(names.Count);
+            string selectedName = names[index];
+            return selectedName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+
+                Random random = new Random();
+                int index = random.Next(titles.Length);
+                string title = titles[index];
+                this.Text = title;
+
+                string name = RandomName();
+                while (name == lastName)
+                {
+                    name = RandomName();
+                }
+                lastName = name;
+                label1.Text = name;
+            }));
+        }
+    }
+}
